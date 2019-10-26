6c387a0c80851121a6c375d54333018f4c9d0591
kusaoisii <dkadd911@gmail.com>
2019-10-26 23:23:02 +0900

    コンポーネントを分離

---
+++ src/containers/StoriesContainer.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+import { getStoryIds } from "../services/hnAPi";
+
+export const  StoriesContainer = () => {
+    // eslint-disable-next-line
+    const [storyIds, setStoryIds] = useState([]);
+
+    useEffect( () => {
+        getStoryIds().then(data => setStoryIds(data))
+    }, []);
+    return (
+        <p>{JSON.stringify(storyIds)}</p>
+    )
+};
+
+export default StoriesContainer;
