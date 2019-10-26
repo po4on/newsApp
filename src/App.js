d4875622eea5209aa8b043d4964a60bf2321a6cb
kusaoisii <dkadd911@gmail.com>
2019-10-26 23:22:40 +0900

    コンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import { getStoryIds } from "./services/hnAPi";
+import React from 'react';
+import { StoriesContainer } from "./containers/StoriesContainer";
 
-export const  App = () => {
-    // eslint-disable-next-line
-    const [storyIds, setStoryIds] = useState([]);
-
-    useEffect( () => {
-        getStoryIds().then(data => setStoryIds(data))
-    }, []);
-    return (
-        <p>{JSON.stringify(storyIds)}</p>
-    )
-};
+export const  App = () => <StoriesContainer />;
 
 export default App;
