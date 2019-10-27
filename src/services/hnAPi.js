f2bbaa57f240f9da8115e050440342f87c2b4b1a
kusaoisii <dkadd911@gmail.com>
2019-10-27 23:47:20 +0900

    見やすいように改行

--- src/services/hnAPi.js
+++ src/services/hnAPi.js
@@ -5,13 +5,17 @@ export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
 export const getStoryIds = async () => {
-    const result = await axios.get(newStoriesUrl).then(({data}) => data);
+    const result = await axios
+        .get(newStoriesUrl)
+        .then(({data}) => data);
 
     return result
 };
 
 export const getStory = async (storyId) => {
-    const result = await axios.get(`${storyUrl + storyId}.json`).then(({data}) => data);
+    const result = await axios
+        .get(`${storyUrl + storyId}.json`)
+        .then(({data}) => data);
 
     return result
 };
